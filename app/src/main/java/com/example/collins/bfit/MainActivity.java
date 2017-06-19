424fe7a8ec1ea2bfe80dd049254a78b1532e41c6
Collins Mwiti <mwiticmutumamwenda@gmail.com>
2017-06-19 12:44:18 +0300

    Added a font in the app

--- app/src/main/java/com/example/collins/bfit/MainActivity.java
+++ app/src/main/java/com/example/collins/bfit/MainActivity.java
@@ -3,18 +3,21 @@ package com.example.collins.bfit;
 
 //imports
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 //class MainActivity
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindMealsButton;
     private EditText mMealEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
 
         mMealEditText = (EditText) findViewById(R.id.mealEditText);
         mFindMealsButton = (Button) findViewById(R.id.findMealsButton);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        //adding a font
+        Typeface ranchoFont = Typeface.createFromAsset(getAssets(), "fonts/Rancho.ttf");
+        mAppNameTextView.setTypeface(ranchoFont);
         //added a listener
         mFindMealsButton.setOnClickListener(new View.OnClickListener() {
             @Override
