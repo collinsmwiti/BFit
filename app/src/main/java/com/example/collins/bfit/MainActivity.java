3f897dd8d95db8e35ae993806e570a3300e1324c
Collins Mwiti <mwiticmutumamwenda@gmail.com>
2017-06-19 11:42:21 +0300

    Created a new activity called Meals Activity and made an intent where one can press a button from MainActivity to MealsActivity

--- app/src/main/java/com/example/collins/bfit/MainActivity.java
+++ app/src/main/java/com/example/collins/bfit/MainActivity.java
@@ -1,13 +1,29 @@
+//package
 package com.example.collins.bfit;
 
-import android.support.v7.app.AppCompatActivity;
+//imports
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+//class MainActivity
 public class MainActivity extends AppCompatActivity {
+    private Button mFindMealsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindMealsButton = (Button) findViewById(R.id.findMealsButton);
+        //added a listener
+        mFindMealsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MealsActivity.class);
+            }
+        });
     }
 }
