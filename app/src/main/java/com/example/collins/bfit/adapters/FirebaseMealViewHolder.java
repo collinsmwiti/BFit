2cb4a4247c7060dafda14a8c3b789a2e9c83aa72
Collins Mwiti <mwiticmutumamwenda@gmail.com>
2017-06-22 11:06:22 +0300

    Added animations in the app

--- app/src/main/java/com/example/collins/bfit/adapters/FirebaseMealViewHolder.java
+++ app/src/main/java/com/example/collins/bfit/adapters/FirebaseMealViewHolder.java
@@ -1,33 +1,24 @@
 package com.example.collins.bfit.adapters;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.collins.bfit.Constants;
 import com.example.collins.bfit.R;
 import com.example.collins.bfit.models.Meal;
-import com.example.collins.bfit.ui.MealDetailActivity;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.example.collins.bfit.util.ItemTouchHelperViewHolder;
 import com.squareup.picasso.Picasso;
-
-import org.parceler.Parcels;
-
-import java.util.ArrayList;
 
 /**
  * Created by collins on 6/21/17.
  */
 
 //class FirebaseMealViewHolder
-public class FirebaseMealViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+public class FirebaseMealViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder {
 
     View mView;
     Context mContext;
@@ -40,6 +31,22 @@ public class FirebaseMealViewHolder extends RecyclerView.ViewHolder implements V
         super(itemView);
         mView = itemView;
         mContext = itemView.getContext();
+    }
+
+    @Override
+    public void onItemSelected() {
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+                R.animator.drag_scale_on);
+        set.setTarget(itemView);
+        set.start();
+    }
+
+    @Override
+    public void onItemClear() {
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+                R.animator.drag_scale_off);
+        set.setTarget(itemView);
+        set.start();
     }
 
     //bind
@@ -56,31 +63,31 @@ public class FirebaseMealViewHolder extends RecyclerView.ViewHolder implements V
 
     }
 
-    @Override
-    public void onClick(View view) {
-        final ArrayList<Meal> meals = new ArrayList<>();
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_MEALS);
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    meals.add(snapshot.getValue(Meal.class));
-                }
-
-                int itemPosition = getLayoutPosition();
-
-                Intent intent = new Intent(mContext, MealDetailActivity.class);
-                intent.putExtra("position", itemPosition + "");
-                intent.putExtra("meals", Parcels.wrap(meals));
-
-                mContext.startActivity(intent);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
-    }
+//    @Override
+//    public void onClick(View view) {
+//        final ArrayList<Meal> meals = new ArrayList<>();
+//        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_MEALS);
+//        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+//
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+//                    meals.add(snapshot.getValue(Meal.class));
+//                }
+//
+//                int itemPosition = getLayoutPosition();
+//
+//                Intent intent = new Intent(mContext, MealDetailActivity.class);
+//                intent.putExtra("position", itemPosition + "");
+//                intent.putExtra("meals", Parcels.wrap(meals));
+//
+//                mContext.startActivity(intent);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//            }
+//        });
+//    }
 
 }
---
+++ app/src/main/java/com/example/collins/bfit/util/ItemTouchHelperViewHolder.java
@@ -0,0 +1,10 @@
+package com.example.collins.bfit.util;
+
+/**
+ * Created by collins on 6/22/17.
+ */
+
+public interface ItemTouchHelperViewHolder {
+    void onItemSelected();
+    void onItemClear();
+}
